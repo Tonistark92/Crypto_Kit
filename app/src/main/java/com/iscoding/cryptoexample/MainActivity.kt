7f2f4e341cce43e8c2ac459c414a6a25b70a4b7c
islam dev <hanyislam270@gmail.com>
2024-06-17 23:29:10 +0300

    basic crypto with key store system and Cipher

---
+++ app/src/main/java/com/iscoding/cryptoexample/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.iscoding.cryptoexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import com.iscoding.cryptoexample.ui.theme.CryptoExampleTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val cipherManager = CipherManager()
+        setContent {
+            CryptoExampleTheme {
+                var messageToEncrypt by remember {
+                    mutableStateOf("")
+                }
+                var messageToDecrypt by remember {
+                    mutableStateOf("")
+                }
+                Column(
+                    modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                ) {
+                    TextField(
+                        value = messageToEncrypt,
+                        onValueChange = { messageToEncrypt = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Encrypt string") }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(onClick = {
+                            val bytes = messageToEncrypt.encodeToByteArray()
+                            val file = File(filesDir, "secret.txt")
+                            if(!file.exists()) {
+                                file.createNewFile()
+                            }
+                            val fos = FileOutputStream(file)
+
+                            messageToDecrypt = cipherManager.encrypt(
+                                bytes = bytes,
+                                outputStream = fos
+                            ).decodeToString()
+                        }) {
+                            Text(text = "Encrypt")
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            val file = File(filesDir, "secret.txt")
+                            messageToEncrypt = cipherManager.decrypt(
+                                inputStream = FileInputStream(file)
+                            ).decodeToString()
+                        }) {
+                            Text(text = "Decrypt")
+                        }
+                    }
+                    Text(text = messageToDecrypt)
+                }
+            }
+        }
+    }
+}
